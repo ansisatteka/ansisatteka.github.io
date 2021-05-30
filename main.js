0a0ad065f0724d303bd269650f03ea3dc39b4b25
Ansis Atteka <ansisatteka@gmail.com>
2021-05-30 02:02:49 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@latest/min/vs' } });
+window.MonacoEnvironment = { getWorkerUrl: () => proxy };
+
+let proxy = URL.createObjectURL(new Blob([`
+	self.MonacoEnvironment = {
+		baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
+	};
+	importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');
+`], { type: 'text/javascript' }));
+
+require(["vs/editor/editor.main"], function () {
+  let editor = monaco.editor.create(document.getElementById('container'), {
+    value: 
+      'console.log("Hello world!");',
+    language: 'javascript',
+    theme: 'vs-dark'
+  });
+});
