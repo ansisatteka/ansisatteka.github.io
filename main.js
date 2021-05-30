7abbacf5d2c939dc60b6aba670c5be6787c50835
Ansis Atteka <ansisatteka@gmail.com>
2021-05-30 04:47:44 +0000

    Third commit

--- main.js
+++ main.js
@@ -16,3 +16,9 @@ require(["vs/editor/editor.main"], function () {
     theme: 'vs-dark'
   });
 });
+
+import http from 'https://unpkg.com/isomorphic-git@beta/http/web/index.js'
+const fs = new LightningFS('fs')
+
+const dir = '/test-clone'
+git.clone({ fs, http, dir, url: 'https://github.com/isomorphic-git/lightning-fs', corsProxy: 'https://cors.isomorphic-git.org' }).then(console.log)
