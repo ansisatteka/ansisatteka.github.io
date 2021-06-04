5f38329d493923ebbb0788824765a12b86e194c6
Ansis Atteka <ansisatteka@gmail.com>
2021-06-04 04:38:11 +0000

    Browser and git

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
-require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@latest/min/vs' } });
-window.MonacoEnvironment = { getWorkerUrl: () => proxy };
+import http from 'https://unpkg.com/isomorphic-git@beta/http/web/index.js' // Initialize isomorphic-git with a file system
 
-let proxy = URL.createObjectURL(new Blob([`
-	self.MonacoEnvironment = {
-		baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
-	};
-	importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');
-`], { type: 'text/javascript' }));
+window.fs = new LightningFS('fs') // I prefer using the Promisified version honestly
+async function SetUpFilesystem() {
+  window.pfs = window.fs.promises
 
-require(["vs/editor/editor.main"], function () {
-  let editor = monaco.editor.create(document.getElementById('container'), {
-    value: 
-      'console.log("Hello world!");',
-    language: 'javascript',
-    theme: 'vs-dark'
+  window.dir = '/tutorial'
+  console.log(dir);
+  try {
+    await pfs.mkdir(dir);
+  } catch (e) {
+
+  }
+  // Behold - it is empty!
+  await pfs.readdir(dir);
+}
+SetUpFilesystem()
+
+function SetUpBrowser() {
+
+  require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@latest/min/vs' } });
+  window.MonacoEnvironment = { getWorkerUrl: () => proxy };
+
+  let proxy = URL.createObjectURL(new Blob([`
+    self.MonacoEnvironment = {
+      baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
+    };
+    importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');
+  `], { type: 'text/javascript' }));
+
+  require(["vs/editor/editor.main"], function () {
+    let editor = monaco.editor.create(document.getElementById('container'), {
+      value: [
+        'function x() {',
+        '\tconsole.log("Hello world!");',
+        '}'
+      ].join('\n'),
+      language: 'javascript',
+      theme: 'vs-dark'
+    });
   });
-});
 
-import http from 'https://unpkg.com/isomorphic-git@beta/http/web/index.js'
-const fs = new LightningFS('fs')
-
-const dir = '/test-clone'
-git.clone({ fs, http, dir, url: 'https://github.com/isomorphic-git/lightning-fs', corsProxy: 'https://cors.isomorphic-git.org' }).then(console.log)
+}
