f5f419bd875e1830d5a9bd9ccf023c0bfa0914b7
Ansis Atteka <ansisatteka@gmail.com>
2021-06-07 20:39:47 +0000

    Refactored out EditorRefocus

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import http from 'https://unpkg.com/isomorphic-git@beta/http/web/index.js'
 
 var editor = null;
 
-
 async function SetUpFilesystem() {
   window.fs = new LightningFS('fs')
   window.pfs = window.fs.promises
@@ -56,14 +55,21 @@ var activeFiles = new Map()
 var activeFile = ""
 
 
-async function activeFileOpen(path, file) {
-  let fullPath = path + file;
-  let oldContent = ""
+function getLangFromFile(file) {
+  if (file.endsWith(".js")) {
+    return "javascript";
+  } else if (file.endsWith(".css")) {
+    return "css";
+  } else if (file.endsWith(".html")) {
+    return "html";
+  }
+  return "text";
+}
 
+async function getGitVersion(file) {
+  let oldContent = ""
   let sha = await git.resolveRef({ fs, dir: '/ansisatteka.github.io.git', ref: 'master' })
-  console.log(sha)
   let commit = await git.readCommit({ fs, dir: '/ansisatteka.github.io.git', oid: sha })
-  console.log(commit.commit.tree)
   let tree = await git.readObject({
     fs,
     dir: '/ansisatteka.github.io.git',
@@ -78,102 +84,92 @@ async function activeFileOpen(path, file) {
     })
     oldContent = new TextDecoder().decode(b.object);
   }
+  return oldContent;
+}
 
-  console.log(oldContent)
-
-  let contents = await pfs.readFile(fullPath, { encoding: "utf8" })
-
-  let lang = "javascript";
-  if (file.endsWith(".js")) {
-    lang = "javascript";
-  } else if (file.endsWith(".css")) {
-    lang = "css";
-  } else if (file.endsWith(".html")) {
-    lang = "html";
+function editorRefocus(ttt) {
+  if (activeTab) {
+    activeTab.elem.id = ""
   }
-  //updateEditor(path, el);
 
-
-  let old_model = monaco.editor.createModel(oldContent, lang);
-  let model = monaco.editor.createModel(contents, lang);
+  activeTab = ttt;
+  ttt.elem.id = "openFileActive";
 
   editor.setModel({
-    original: old_model,
-    modified: model
+    original: ttt.old_model,
+    modified: ttt.model
   });
+}
 
+async function activeFileOpen(path, file) {
+  let fullPath = path + file;
 
   if (!activeFiles.has(fullPath)) {
+    /* File is not yet open */
+
+    let oldContent = await getGitVersion(file)
+    let contents = await pfs.readFile(fullPath, { encoding: "utf8" })
+
+    let lang = getLangFromFile(file);
+    let old_model = monaco.editor.createModel(oldContent, lang);
+    let model = monaco.editor.createModel(contents, lang);
+
+
     let newTab = document.createElement("div")
     let fileSpan = document.createElement("span")
+    let closeButton = document.createElement("button")
 
     fileSpan.innerHTML = fullPath;
-
     newTab.className = "openFile";
 
-
+    let ttt = { elem: newTab, model: model, old_model: old_model }
     fileSpan.addEventListener("click", function () {
-
-      if (activeTab) {
-        activeTab.id = "";
-      }
-      activeTab = newTab;
-      newTab.id = "openFileActive";
-      editor.setModel({
-        original: old_model,
-        modified: model
-      });
+      editorRefocus(ttt)
     })
-    let closeButton = document.createElement("button")
     closeButton.innerHTML = "x"
     closeButton.addEventListener("click", function () {
       pfs.writeFile(fullPath, model.getValue())
       openFilesContainer.removeChild(newTab);//TODO: does this leave dangling button html element?
       activeFiles.delete(fullPath)
+      //TODO: need to call editorRefocus() with the file that was previously open
     })
     newTab.appendChild(fileSpan)
 
     newTab.appendChild(closeButton)
     openFilesContainer.appendChild(newTab);
-    activeFiles.set(fullPath, { elem: newTab })
-    if (activeTab) {
-      activeTab.id = ""
-    }
-    activeTab = newTab;
-    newTab.id = "openFileActive";
+    activeFiles.set(fullPath, ttt)
+    editorRefocus(ttt)
   } else {
-    let elem = activeFiles.get(fullPath).elem
-    if (activeTab) {
-      activeTab.id = ""
-    }
-    activeTab = elem;
-    elem.id = "openFileActive";
+    /* File is already open. Just refocus */
+    let ttt = activeFiles.get(fullPath)
+    editorRefocus(ttt)
+
   }
 
 }
 
 async function fileExplorerInit(explorer, path) {
-  let ret1 = await pfs.readdir(path);
+  let files = await pfs.readdir(path);
 
-  for (let el of ret1) {
+  for (let file of files) {
     /* Ignore GIT internal directories */
-    if (el == ".git") {
+    if (file == ".git") {
       continue;
     }
 
-    let fullPath = path + el
-    let ret2 = await pfs.stat(fullPath);
+    let fullPath = path + file
+    let finfo = await pfs.stat(fullPath);
 
     let entry = document.createElement('div');
     entry.innerHTML = fullPath;
-    if (ret2.type == "file") {
+    if (finfo.type == "file") {
       entry.addEventListener("click", async function () {
-        activeFileOpen(path, el);
+        activeFileOpen(path, file);
       });
     }
     explorer.appendChild(entry);
     /* Recursively go into child directories */
-    if (ret2.type == "dir") {
+    if (finfo.type == "dir") {
       await fileExplorerInit(explorer, fullPath + "/");
     }
 
